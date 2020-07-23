05acde63a044141d55d90dc3a46fe7ee461c3562
Abin-creator <65009239+Abin-creator@users.noreply.github.com>
2020-07-23 15:54:49 +0700

    Update SpeakGoodbye.js

--- Mod-4-Soln/SpeakGoodbye.js
+++ Mod-4-Soln/SpeakGoodbye.js
@@ -11,7 +11,7 @@
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-var speakWord = "Good Bye";
+var speakWord = "GoodBye";
 var byeSpeaker = {speak: function(name){
     console.log(speakWord + " " + name);
 }};
